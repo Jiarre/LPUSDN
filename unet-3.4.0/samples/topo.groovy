90bad22a41544262f22d3eaff70bbaa0657febcb
Jiarre <38040300+Jiarre@users.noreply.github.com>
2023-06-15 08:35:20 +0200

    Initial commit

---
+++ unet-3.4.0/samples/topo.groovy
@@ -0,0 +1,33 @@
+import org.arl.fjage.*
+
+///////////////////////////////////////////////////////////////////////////////
+// display documentation
+
+println '''
+SDN network
+--------------
+
+Controller
+|  |  |
+1  2  SINK
+   |
+   3
+
+'''
+
+///////////////////////////////////////////////////////////////////////////////
+// simulator configuration
+
+platform = RealTimePlatform   // use real-time mode
+
+// run the simulation forever
+simulate {
+  node '10',address:10, location: [ 0.km, 0.km, 0.m], web: 8090, api: 1110, stack: "$home/etc/setup"
+  node '1',address:1, location: [ 121.m,  137.m, -10.m],  web: 8081, api: 1101, stack: "$home/etc/setup"
+  node '2', location: [350.m, -232.m, -15.m],  web: 8082, api: 1102, stack: "$home/etc/setup"
+  node '3', location: [500.m,  140.m,  -5.m],  web: 8083, api: 1103, stack: "$home/etc/setup"
+  node '4', location: [601.m,  120.m,  -5.m],  web: 8084, api: 1104, stack: "$home/etc/setup"
+
+
+
+}
